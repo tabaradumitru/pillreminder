b5c720de2b00f0b4a586a36a2c92a8b6cf595ba9
Dumitru Tabara <dumitru.tabara@codwer.com>
2020-01-03 17:46:47 +0200

    Initial Commit

---
+++ PillReminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PillReminder
+//
+//  Created by Dumitru Tabara on 1/3/20.
+//  Copyright © 2020 Dumitru Tabara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
